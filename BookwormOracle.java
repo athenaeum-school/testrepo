ab1203661ad0d4cf86b006aa53b7f7f8ba7bfae8
アセナエウム <school@athenaeum-society.com>
2014-11-01 18:31:59 +0900

    test

---
+++ BookwormOracle.java
@@ -0,0 +1,9 @@
+package com.as;
+
+public class BookwormOracle implements Oracle {
+
+    public String defineMeaningOfLife() {
+        return "Encyclopedias are a waste of money - use the Internet";
+    }	
+	
+}
